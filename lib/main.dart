4cbd3a90ab44d6578d8fa0254c19c22f43e10b95
Ashrafulalkh <ashrafulkhan194@gmail.com>
2024-05-01 00:28:51 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Center(
